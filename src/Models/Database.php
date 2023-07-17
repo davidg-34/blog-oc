866ed1525db9d7ddb2602cd73925324d4bef345c
Samantha Halfon Faw <samantha.halfon@gmail.com>
2023-07-17 13:04:16 +0200

    fix new post

--- src/Models/Database.php
+++ src/Models/Database.php
@@ -9,10 +9,10 @@ class Database {
     public static function initDatabase() {
         if (self::$db != null) return;
         try {
-            self::$db = new \PDO('mysql:host=localhost;dbname=blog;charset=utf8', 'root', 'root');
+            self::$db = new \PDO('mysql:host=localhost;dbname=oc-david-p5;charset=utf8', 'root');
             self::$db->setAttribute(\PDO::ATTR_ERRMODE, \PDO::ERRMODE_EXCEPTION);
         } catch (\Exception $e) {
-             
-        }        
+
+        }
     }
 }
