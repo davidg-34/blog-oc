fd1a3c6f2b6c975ceb67210800da807c6122c2a2
Samantha Halfon Faw <samantha.halfon@gmail.com>
2023-07-24 19:01:57 +0200

    fix mailer

--- src/Controllers/ContactController.php
+++ src/Controllers/ContactController.php
@@ -14,50 +14,58 @@ require 'vendor/autoload.php';
 
 class ContactController extends Controller{
     public function index(){
-        if (count($_POST)) {             
+        if (count($_POST)) {
             $request = new \App\Request();
             $firstname = $request->getParam("firstname");
             $lastname = $request->getParam("lastname");
             $email = $request->getParam("email");
             $message =  $request->getParam("message");
-            sendContactMail()    
+            // TO DO gestion d'erreur
+            if  (false) {
+                return "error";
+            }
+            $this->sendContactMail($email, $firstname, $lastname, $message);
         }
         $this->render('contact.html.twig');
+    }
+
+    private function sendContactMail($email, $firstname, $lastname, $message) {
+        //Create an instance; passing `true` enables exceptions
+        $mail = new PHPMailer(true);
+
+        try {
+            //Server settings
+            $mail->SMTPDebug = false;                      //Enable verbose debug output
+            $mail->isSMTP();                                            //Send using SMTP
+            $mail->Host       = 'smtp.gmail.com';                       //Set the SMTP server to send through
+            $mail->SMTPAuth   = true;                                   //Enable SMTP authentication
+            $mail->Username   = 'david.guion.web@gmail.com';            //SMTP username
+            $mail->Password   = 'oufxalgnhnalzgkt';                     //SMTP password
+            $mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS;            //Enable implicit TLS encryption
+            $mail->Port       = 465;                                    //TCP port to connect to; use 587 if you have set `SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS`
+
+            //Recipients
+            $mail->setFrom('david.guion.web@gmail.com', 'Blog de David');
+            $mail->addAddress('david.guion.web@gmail.com', $firstname. ' ' . $lastname);     //Add a recipient
+
+            //Content
+            $mail->isHTML(true);                                        //Set email format to HTML
+            $mail->Subject = 'Message de contact sur le blog';
+            $mail->Body    = 'Message réçu de<br>Nom : ' . $firstname . ' ' . $lastname .'<br>Email : ' . $email . '<br>Message : ' . $message;
+            //$mail->AltBody = 'This is the body in plain text for non-HTML mail clients';
+
+            $mail->send();
+            // echo 'Message has been sent';
+        } catch (Exception $e) {
+            echo "Message could not be sent. Mailer Error: {$mail->ErrorInfo}";
+        }
+
+
     }
 }
 
 
-//Create an instance; passing `true` enables exceptions
-$mail = new PHPMailer(true);
-
-try {
-    //Server settings
-    $mail->SMTPDebug = SMTP::DEBUG_SERVER;                      //Enable verbose debug output
-    $mail->isSMTP();                                            //Send using SMTP
-    $mail->Host       = 'smtp.gmail.com';                       //Set the SMTP server to send through
-    $mail->SMTPAuth   = true;                                   //Enable SMTP authentication
-    $mail->Username   = 'david.guion.web@gmail.com';            //SMTP username
-    $mail->Password   = 'oufxalgnhnalzgkt';                     //SMTP password
-    $mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS;            //Enable implicit TLS encryption
-    $mail->Port       = 465;                                    //TCP port to connect to; use 587 if you have set `SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS`
-
-    //Recipients
-    $mail->setFrom('from@example.com', 'Mailer');
-    $mail->addAddress($_POST['email'], $_POST['lastname']);     //Add a recipient
-
-    //Content
-    $mail->isHTML(true);                                        //Set email format to HTML
-    $mail->Subject = 'Here is the subject';
-    $mail->Body    = 'Nom : '.$lastname. 'email : '.$email. 'Message : '.$message.'</b>';
-    //$mail->AltBody = 'This is the body in plain text for non-HTML mail clients';
-
-    $mail->send();
-    echo 'Message has been sent';
-} catch (Exception $e) {
-    echo "Message could not be sent. Mailer Error: {$mail->ErrorInfo}";
-}
 
 
 
-    
 
