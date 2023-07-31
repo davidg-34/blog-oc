9e470f7d226ac7371c310328434c9b830f36ec14
Samantha Halfon Faw <samantha.halfon@gmail.com>
2023-07-31 15:39:11 +0200

    fix blog dernieres corrections

--- src/Controllers/Controller.php
+++ src/Controllers/Controller.php
@@ -6,14 +6,17 @@ class Controller {
     protected $twigLoader;
     protected $twig;
 
-    public function __construct() 
+    public function __construct()
     {
         $this->twigLoader = new \Twig\Loader\FilesystemLoader('src/templates');
         $this->twig = new \Twig\Environment($this->twigLoader);
     }
 
-    public function render($template, $params = []) 
+    public function render($template, $params = [])
     {
+        $session = new \App\Session();
+        $params["userId"] = $session->get("userId");
+        $params["userName"] = $session->get("userName");
         echo $this->twig->render($template, $params);
     }
 
--- src/Models/Database.php
+++ src/Models/Database.php
@@ -1,37 +1,24 @@
 <?php
 
-/* namespace App\Models;
-
-class Database {
-
-    protected static $db;
-
-    public static function initDatabase() {
-        if (self::$db != null) return;
-        try {
-            self::$db = new \PDO('mysql:host=localhost;dbname=oc-david-p5;charset=utf8', 'root');
-            self::$db->setAttribute(\PDO::ATTR_ERRMODE, \PDO::ERRMODE_EXCEPTION);
-        } catch (\Exception $e) {
-
-        }
-    }
-}
- */
-
  namespace App\Models;
- 
+
  class Database {
- 
+
      protected static $db;
- 
+
      public static function initDatabase() {
-         if (self::$db != null) return;
+        require_once "./.env.php";
+         if (self::$db != null) {
+            return;
+         }
          try {
-             self::$db = new \PDO('mysql:host=localhost;dbname=blog;charset=utf8', 'root', 'root');
+             self::$db = new \PDO(
+                'mysql:host=' . $config['database']['hostname'] . ';dbname=' . $config['database']['dbname'] . ';charset=utf8',
+                $config['database']['username'], $config['database']['password']
+            );
              self::$db->setAttribute(\PDO::ATTR_ERRMODE, \PDO::ERRMODE_EXCEPTION);
          } catch (\Exception $e) {
-              
-         }        
+            print_r($e);
+         }
      }
  }
- 
